7c6a141657f0bc9848b87b919ed0c963eab34695
mike nodding <m.nodding@murdoch.edu.au>
2019-03-12 15:45:02 +0800

    navbar template alterations

--- lib.php
+++ lib.php
@@ -62,14 +62,15 @@ function theme_sonofbooster_get_main_scss_content($theme) {
     if (empty($boosttheme)) {
         $boosttheme = theme_config::load('boost'); // Needs to be the Boost theme so that we get its settings.
     }
-    if ($boosttheme->settings->preset == 'default.scss') {
-        /* Use our own default.scss as the Boost default.scss redefines $theme-colors instead of merging with
-           map-merge as shown in _variables.css.  The method 'theme_boost_get_main_scss_content()' only looks
-           at the 'preset' setting.  If this changes then adapt.*/
-        $scss = file_get_contents($CFG->dirroot.'/theme/sonofbooster/scss/sob_presets/default.scss');
-    } else {
+   // something not quite right here
+  //  if ($boosttheme->settings->preset == 'default.scss') {
+          // Use our own default.scss as the Boost default.scss redefines $theme-colors instead of merging with
+          // map-merge as shown in _variables.css.  The method 'theme_boost_get_main_scss_content()' only looks
+          // at the 'preset' setting.  If this changes then adapt.*/
+       // $scss = file_get_contents($CFG->dirroot.'/theme/sonofbooster/scss/sob_presets/default.scss');
+   // } else {
         $scss = theme_boost_get_main_scss_content($boosttheme);
-    }
+   // }
 
     $scss .= file_get_contents($CFG->dirroot.'/theme/sonofbooster/scss/sonofbooster.scss');
 
