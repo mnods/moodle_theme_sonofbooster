659d0e8c20fba69643ad4435367ff07f5f27e2e7
mike nodding <m.nodding@murdoch.edu.au>
2019-03-18 10:24:32 +0800

    nav class tute

--- classes/admin_setting_configfontsizes.php
+++ classes/admin_setting_configfontsizes.php
@@ -1,3 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * sonofbooster theme.
+ *
+ * @package    theme_sonofbooster
+ * @copyright  &copy; 2018-onwards G J Barnard.
+ * @author     G J Barnard - {@link http://moodle.org/user/profile.php?id=442195}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ */
+
 class admin_setting_configfontsizes extends admin_setting_configtext {
     public function __construct($name, $visiblename, $description, $defaultsetting) {
             parent::__construct($name, $visiblename, $description, $defaultsetting, PARAM_TEXT);
@@ -29,7 +54,7 @@ public function validate($data) {
             $data = self::decode_from_db($data);
             // Ensure there are enough settings.
             if (count($data) != 7) {
-                $validated = get_string('fontsizessizeerror', 'theme_moobytes');
+                $validated = get_string('fontsizessizeerror', 'theme_sonofbooster');
             } else {
                 // Ensure that heading multipliers decrease.
                 $index = 2;
